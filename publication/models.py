0146f17dcef90b084094f90a71f110f2aaea0f21
kupcplab <kupcplab@gmail.com>
2026-08-18 16:07:53 +0900

    Update models.py

--- publication/models.py
+++ publication/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
 
+
 class Publication(models.Model):
     publication = CKEditor5Field('출판 목록', config_name='default')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Publications"
+
+
+class Book(models.Model):
+    book = CKEditor5Field('도서 목록', config_name='default')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Books"
